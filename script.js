c08adc79fb41f4cc66c196a0277c883df14f1037
46tcampbell <157092336+46tcampbell@users.noreply.github.com>
2025-09-16 16:54:45 -0500

    Make the calculator work
    Overall functionality of the calculator is working
    as designed. Need to go back through and work on
    edge cases as there are some bugs in the
    functionality.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let calcNum1 = null;
-let calcOperator;
-let calcNum2 = null;
+let calcNum1 = undefined;
+let calcOperator = undefined;
+let calcNum2 = undefined;
 
 function add(num1, num2) {
   return num1 + num2;
@@ -33,7 +33,7 @@ function operate(num1, num2, operator) {
 const calcDisplay = document.querySelector('#calc-display');
 const btn0 = document.querySelector('#btn0');
 btn0.addEventListener('click', () => {
-  if (calcDisplay.textContent === '0') {
+  if (calcDisplay.textContent === '0' || (calcNum1 && calcNum2 === undefined)) {
     calcDisplay.textContent = '0';
   } else {
     calcDisplay.textContent += '0';
@@ -42,8 +42,14 @@ btn0.addEventListener('click', () => {
 
 const btn1 = document.querySelector('#btn1');
 btn1.addEventListener('click', () => {
-  if (calcDisplay.textContent === '0') {
+  if (
+    calcDisplay.textContent === '0' ||
+    (calcNum1 && calcOperator === undefined && calcNum2 === undefined)
+  ) {
     calcDisplay.textContent = '1';
+  } else if (calcNum1 && calcOperator && calcNum2 === undefined) {
+    calcDisplay.textContent = '1';
+    calcNum2 = null;
   } else {
     calcDisplay.textContent += '1';
   }
@@ -51,8 +57,14 @@ btn1.addEventListener('click', () => {
 
 const btn2 = document.querySelector('#btn2');
 btn2.addEventListener('click', () => {
-  if (calcDisplay.textContent === '0') {
+  if (
+    calcDisplay.textContent === '0' ||
+    (calcNum1 && calcOperator === undefined && calcNum2 === undefined)
+  ) {
     calcDisplay.textContent = '2';
+  } else if (calcNum1 && calcOperator && calcNum2 === undefined) {
+    calcDisplay.textContent = '2';
+    calcNum2 = null;
   } else {
     calcDisplay.textContent += '2';
   }
@@ -60,8 +72,14 @@ btn2.addEventListener('click', () => {
 
 const btn3 = document.querySelector('#btn3');
 btn3.addEventListener('click', () => {
-  if (calcDisplay.textContent === '0') {
+  if (
+    calcDisplay.textContent === '0' ||
+    (calcNum1 && calcOperator === undefined && calcNum2 === undefined)
+  ) {
     calcDisplay.textContent = '3';
+  } else if (calcNum1 && calcOperator && calcNum2 === undefined) {
+    calcDisplay.textContent = '3';
+    calcNum2 = null;
   } else {
     calcDisplay.textContent += '3';
   }
@@ -69,8 +87,14 @@ btn3.addEventListener('click', () => {
 
 const btn4 = document.querySelector('#btn4');
 btn4.addEventListener('click', () => {
-  if (calcDisplay.textContent === '0') {
+  if (
+    calcDisplay.textContent === '0' ||
+    (calcNum1 && calcOperator === undefined && calcNum2 === undefined)
+  ) {
     calcDisplay.textContent = '4';
+  } else if (calcNum1 && calcOperator && calcNum2 === undefined) {
+    calcDisplay.textContent = '4';
+    calcNum2 = null;
   } else {
     calcDisplay.textContent += '4';
   }
@@ -78,8 +102,14 @@ btn4.addEventListener('click', () => {
 
 const btn5 = document.querySelector('#btn5');
 btn5.addEventListener('click', () => {
-  if (calcDisplay.textContent === '0') {
+  if (
+    calcDisplay.textContent === '0' ||
+    (calcNum1 && calcOperator === undefined && calcNum2 === undefined)
+  ) {
     calcDisplay.textContent = '5';
+  } else if (calcNum1 && calcOperator && calcNum2 === undefined) {
+    calcDisplay.textContent = '5';
+    calcNum2 = null;
   } else {
     calcDisplay.textContent += '5';
   }
@@ -87,8 +117,14 @@ btn5.addEventListener('click', () => {
 
 const btn6 = document.querySelector('#btn6');
 btn6.addEventListener('click', () => {
-  if (calcDisplay.textContent === '0') {
+  if (
+    calcDisplay.textContent === '0' ||
+    (calcNum1 && calcOperator === undefined && calcNum2 === undefined)
+  ) {
     calcDisplay.textContent = '6';
+  } else if (calcNum1 && calcOperator && calcNum2 === undefined) {
+    calcDisplay.textContent = '6';
+    calcNum2 = null;
   } else {
     calcDisplay.textContent += '6';
   }
@@ -96,8 +132,14 @@ btn6.addEventListener('click', () => {
 
 const btn7 = document.querySelector('#btn7');
 btn7.addEventListener('click', () => {
-  if (calcDisplay.textContent === '0') {
+  if (
+    calcDisplay.textContent === '0' ||
+    (calcNum1 && calcOperator === undefined && calcNum2 === undefined)
+  ) {
     calcDisplay.textContent = '7';
+  } else if (calcNum1 && calcOperator && calcNum2 === undefined) {
+    calcDisplay.textContent = '7';
+    calcNum2 = null;
   } else {
     calcDisplay.textContent += '7';
   }
@@ -105,8 +147,14 @@ btn7.addEventListener('click', () => {
 
 const btn8 = document.querySelector('#btn8');
 btn8.addEventListener('click', () => {
-  if (calcDisplay.textContent === '0') {
+  if (
+    calcDisplay.textContent === '0' ||
+    (calcNum1 && calcOperator === undefined && calcNum2 === undefined)
+  ) {
     calcDisplay.textContent = '8';
+  } else if (calcNum1 && calcOperator && calcNum2 === undefined) {
+    calcDisplay.textContent = '8';
+    calcNum2 = null;
   } else {
     calcDisplay.textContent += '8';
   }
@@ -114,23 +162,142 @@ btn8.addEventListener('click', () => {
 
 const btn9 = document.querySelector('#btn9');
 btn9.addEventListener('click', () => {
-  if (calcDisplay.textContent === '0') {
+  if (
+    calcDisplay.textContent === '0' ||
+    (calcNum1 && calcOperator === undefined && calcNum2 === undefined)
+  ) {
     calcDisplay.textContent = '9';
+  } else if (calcNum1 && calcOperator && calcNum2 === undefined) {
+    calcDisplay.textContent = '9';
+    calcNum2 = null;
   } else {
     calcDisplay.textContent += '9';
+  }
+});
+
+const btnDivide = document.querySelector('#btnDivide');
+btnDivide.addEventListener('click', () => {
+  if (
+    calcDisplay.textContent &&
+    calcNum1 === undefined &&
+    calcNum2 === undefined &&
+    calcOperator === undefined
+  ) {
+    calcNum1 = Number(calcDisplay.textContent);
+    calcOperator = 'forwardSlash';
+    // calcDisplay.textContent += ' /';
+  } else if (
+    calcDisplay.textContent &&
+    calcNum1 &&
+    calcNum2 === null &&
+    calcOperator
+  ) {
+    calcNum2 = Number(calcDisplay.textContent);
+    calcDisplay.textContent = operate(calcNum1, calcNum2, calcOperator);
+    calcNum1 = Number(calcDisplay.textContent);
+    calcNum2 = undefined;
+    calcOperator = 'forwardSlash';
+  }
+});
+
+const btnMultiply = document.querySelector('#btnMultiply');
+btnMultiply.addEventListener('click', () => {
+  if (
+    calcDisplay.textContent &&
+    calcNum1 === undefined &&
+    calcNum2 === undefined &&
+    calcOperator === undefined
+  ) {
+    calcNum1 = Number(calcDisplay.textContent);
+    calcOperator = 'asterisk';
+    // calcDisplay.textContent += ' /';
+  } else if (
+    calcDisplay.textContent &&
+    calcNum1 &&
+    calcNum2 === null &&
+    calcOperator
+  ) {
+    calcNum2 = Number(calcDisplay.textContent);
+    calcDisplay.textContent = operate(calcNum1, calcNum2, calcOperator);
+    calcNum1 = Number(calcDisplay.textContent);
+    calcNum2 = undefined;
+    calcOperator = 'asterisk';
+  }
+});
+
+const btnAdd = document.querySelector('#btnAdd');
+btnAdd.addEventListener('click', () => {
+  if (
+    calcDisplay.textContent &&
+    calcNum1 === undefined &&
+    calcNum2 === undefined &&
+    calcOperator === undefined
+  ) {
+    calcNum1 = Number(calcDisplay.textContent);
+    calcOperator = 'plus';
+    // calcDisplay.textContent += ' /';
+  } else if (
+    calcDisplay.textContent &&
+    calcNum1 &&
+    calcNum2 === null &&
+    calcOperator
+  ) {
+    calcNum2 = Number(calcDisplay.textContent);
+    calcDisplay.textContent = operate(calcNum1, calcNum2, calcOperator);
+    calcNum1 = Number(calcDisplay.textContent);
+    calcNum2 = undefined;
+    calcOperator = 'plus';
+  }
+});
+
+const btnSubtract = document.querySelector('#btnSubtract');
+btnSubtract.addEventListener('click', () => {
+  if (
+    calcDisplay.textContent &&
+    calcNum1 === undefined &&
+    calcNum2 === undefined &&
+    calcOperator === undefined
+  ) {
+    calcNum1 = Number(calcDisplay.textContent);
+    calcOperator = 'minus';
+    // calcDisplay.textContent += ' /';
+  } else if (
+    calcDisplay.textContent &&
+    calcNum1 &&
+    calcNum2 === null &&
+    calcOperator
+  ) {
+    calcNum2 = Number(calcDisplay.textContent);
+    calcDisplay.textContent = operate(calcNum1, calcNum2, calcOperator);
+    calcNum1 = Number(calcDisplay.textContent);
+    calcNum2 = undefined;
+    calcOperator = 'minus';
+  }
+});
+
+const btnEquals = document.querySelector('#btnEquals');
+btnEquals.addEventListener('click', () => {
+  if (
+    calcDisplay.textContent &&
+    calcNum1 &&
+    (calcNum2 === undefined || calcNum2 === null) &&
+    calcOperator
+  ) {
+    calcNum2 = Number(calcDisplay.textContent);
+    calcDisplay.textContent = operate(calcNum1, calcNum2, calcOperator);
+    calcNum1 = undefined;
+    calcNum2 = undefined;
   }
 });
 
 const clear = document.querySelector('#clear');
 clear.addEventListener('click', () => {
   calcDisplay.textContent = '';
+  calcNum1 = undefined;
+  calcNum2 = undefined;
+  calcOperator = undefined;
 });
 
-console.log(operate(calcNum1, calcNum2, calcOperator));
+// console.log(operate(calcNum1, calcNum2, calcOperator));
 
-//   below was functionality on Button 0, but I don't think I'll actually need this
-//   if (calcNum1 === null && calcNum2 === null) {
-//     calcNum1 = 0;
-//   } else if ((calcNum1 || calcNum1 === 0) && calcNum2 === null) {
-//     calcNum2 = 0;
-//   }
+// console.log(calcNum1, calcNum2, calcOperator)
