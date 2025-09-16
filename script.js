47550c60a2f355833172fd5a193ce82e452f44ac
46tcampbell <157092336+46tcampbell@users.noreply.github.com>
2025-09-16 12:25:20 -0500

    Add calculator operation function
    I used an if else statement along with a string
    name to represent the operator as I did not know a
    way to use the actual operator symbol without
    javascript thinking it was the actual operator.
    
    Functionality works, but there may be an option for
    refactoring later.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let calcNum1;
-let calcOperator;
-let calcNum2;
+let calcNum1 = 10;
+let calcOperator = 'forwardSlash';
+let calcNum2 = 10;
 
 function add(num1, num2) {
   return num1 + num2;
@@ -17,3 +17,17 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
   return num1 / num2;
 }
+
+function operate(num1, num2, operator) {
+  if (operator === 'plus') {
+    return add(num1, num2);
+  } else if (operator === 'minus') {
+    return subtract(num1, num2);
+  } else if (operator === 'asterisk') {
+    return multiply(num1, num2);
+  } else if (operator === 'forwardSlash') {
+    return divide(num1, num2);
+  }
+}
+
+console.log(operate(calcNum1, calcNum2, calcOperator));
